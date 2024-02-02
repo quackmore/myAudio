aad46baa64a6541c6bd93f62d7fa04ca0e41df70
quackmore <quackmore-ff@yahoo.com>
2024-02-02 16:37:00 +0100

    first production config

--- api/home/home.route.js
+++ api/home/home.route.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   // using ejs for rendering
-  res.render('index', { prefs: { audioContactDev: `${cfg.get('audioOut.contactDevice')}`, audioContactRel: `${cfg.get('audioOut.contactRelay')}` } });
+  res.render('index', { prefs: { audioContactDev: `${cfg.get('player.contactDevice')}`, audioContactRel: `${cfg.get('player.contactRelay')}` } });
 });
 
 module.exports = router;
