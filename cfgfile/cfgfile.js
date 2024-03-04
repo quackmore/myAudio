47c3c5edce1c2839597ad2d7385e24647d1aea55
quackmore <quackmore-ff@yahoo.com>
2024-03-04 21:21:12 +0100

    moved myAudio.json file to $HOME/.myAudio/config.json

--- cfgfile/cfgfile.js
+++ cfgfile/cfgfile.js
@@ -2,18 +2,28 @@ const log = require('../logger')
 const path = require('path');
 const pkg = require('../package.json');
 const fs = require('fs');
+const cfgFolder = require('os').homedir() + "/." + pkg.name;
 
 
 module.exports = {
+  init: () => {
+    try {
+      if (!fs.existsSync(cfgFolder)) {
+        fs.mkdirSync(cfgFolder);
+      }
+    } catch (err) {
+      log.error(err.message);
+    }
+  },
   read: () => {
     try {
-      let file_content = fs.readFileSync(path.join(__dirname, `../config/${pkg.name}.json`));
+      let file_content = fs.readFileSync(path.join(cfgFolder, "config.json"));
       return JSON.parse(file_content);
     } catch (err) {
       log.error(err.message)
       if (err.code === 'ENOENT') {
         try {
-          fs.writeFileSync(path.join(__dirname, `../config/${pkg.name}.json`), JSON.stringify({}, null, 4));
+          fs.writeFileSync(path.join(cfgFolder, "config.json"), JSON.stringify({}, null, 4));
         } catch (err) {
           log.error(err.message)
         }
@@ -23,7 +33,7 @@ module.exports = {
   },
   save: (content) => {
     try {
-      fs.writeFileSync(path.join(__dirname, `../config/${pkg.name}.json`), JSON.stringify(content, null, 4));
+      fs.writeFileSync(path.join(cfgFolder, "config.json"), JSON.stringify(content, null, 4));
     } catch (err) {
       log.error(err.message)
     }
--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const log = require('./logger')
 const httpd = require('./httpd/httpd')
 const config = require('config');
 const packageJSON = require("./package.json");
+const cfgFile = require("./cfgfile");
 const mpd = require("./mpd");
 
 log.info(`${packageJSON.name} started`);
+cfgFile.init();
 mpd.start();
 
 // graceful shutdown
