901beb8a84572eca826340d519ae86005776d15e
quackmore <quackmore-ff@yahoo.com>
2024-06-27 18:12:31 +0200

    early podcast

--- api/podcast/podcast.route.js
+++ api/podcast/podcast.route.js
@@ -1,12 +1,30 @@
 const express = require('express');
-const cfg = require('config');
 const log = require('../../logger');
-const cast = require('../../podcast');
+const fs = require('fs');
+const cfgFilesRoot = require('../../utils/cfgFilesRoot');
+const path = require('path');
+
+const podcastsFile = path.join(cfgFilesRoot(), "/podcasts/podcasts.json");
 const router = express.Router();
 
 router.get('/', async function (req, res, next) {
-  cast.print();
-  res.sendStatus(200);
+  try {
+    let file_content = fs.readFileSync(podcastsFile, "utf-8");
+    res.json(JSON.parse(file_content));
+  } catch (err) {
+    log.error(err.message)
+    res.status(500).send(err.message);
+  }
+});
+
+router.post('/', async function (req, res, next) {
+  try {
+    fs.writeFileSync(podcastsFile, JSON.stringify(req.body));
+    res.sendStatus(200);
+  } catch (err) {
+    log.error(err.message)
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
--- podcast/podcast.js
+++ podcast/podcast.js
@@ -5,7 +5,7 @@ let parser = new podcastParser();
 
 async function print() {
 
-  parser.parseURL('https://backr24.ilsole24ore.com/podcast/24mattino.xml', function (err, feed) {
+  parser.parseURL('https://www.spreaker.com/show/4255213/episodes/feed', function (err, feed) {
     console.log(feed);
   })
 }
