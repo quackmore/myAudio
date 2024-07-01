84c6aa9ebcc125ce870f7ebf3970631a61b753a7
quackmore <quackmore-ff@yahoo.com>
2024-07-01 18:07:03 +0200

    bug fixing

--- podcast/podcast.js
+++ podcast/podcast.js
@@ -4,7 +4,7 @@ const RSSParser = require('rss-parser');
 async function getEpisodes(feedUrl) {
   let parser = new RSSParser();
   let feed = await parser.parseURL(feedUrl);
-  return feed.items.map(item => ({ title: item.title, url: item.link, contentSnippet: item.contentSnippet, date: item.isoDate.substring(0,10) }));
+  return feed.items.map(item => ({ title: item.title, url: item.enclosure.url, duration: item.itunes.duration, contentSnippet: item.contentSnippet, date: item.isoDate.substring(0, 10) }));
 }
 
 module.exports = {
